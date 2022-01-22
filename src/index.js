0826162a8eae55c302ca511fe38cc77f86457dd1
emilytangg <emilytang3012@gmail.com>
2022-01-22 14:24:22 +1100

    Made the selected item in the move list bold

--- src/index.js
+++ src/index.js
@@ -88,7 +88,7 @@ class Game extends React.Component {
         'Go to move #' + move : 'Go to game start';
       return (
         <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          <button className="moveList" onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
       );
     });
